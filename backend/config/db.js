b2c063f62137df4b101db2482a1b0adc2f3c1046
KaranGupta_2006 <guptakaran.port@gmail.com>
2026-01-27 10:40:12 +0530

    Schema fixed

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,12 +3,12 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const connectDB= async () =>{
+const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URL || 'mongodb+srv://guptakaranport:karang2006@cluster0.gapyepy.mongodb.net/');
 }
 
 connectDB()
-.then(() => console.log('MongoDB connected'))
-.catch(err => console.error('MongoDB connection error:', err));
+    .then(() => console.log('MongoDB connected'))
+    .catch(err => console.error('MongoDB connection error:', err));
 
 export default connectDB;
