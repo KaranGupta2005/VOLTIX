6a84181bc2743e73f30c71158c432d2fb855223e
varunchauhan12 <varunchauhan_mc24b06_058@dtu.ac.in>
2026-01-30 13:35:01 +0530

    added login and sign up page ui

---
+++ my-app/app/(pages)/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginForm } from "@/app/components/loginForm";
+import { ShaderRipple } from "@/components/ui/shader-ripple";
+export default function Page() {
+  return (
+    <div className="h-screen flex items-center justify-center relative">
+      <div className="w-full z-10 relative max-w-sm">
+        <LoginForm />
+      </div>
+      <ShaderRipple className="absolute -z-0 inset-0 h-screen" />
+    </div>
+  );
+}
